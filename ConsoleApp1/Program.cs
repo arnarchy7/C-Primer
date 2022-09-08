5768814d334938014e8dde99fdf543755f31742c
arnarchy7 <arnarchy7@gmail.com>
2022-09-08 14:14:28 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1;
+
+//Dæmi 1
+
+/*
+List<int> numbers = new List<int>();
+
+string input = "x";
+
+while (input != "")
+{
+    Console.Write("Input a number: ");
+
+    input = Console.ReadLine();
+
+    if(input == "") { 
+    
+        Console.WriteLine(numbers.Max());
+
+        Console.WriteLine(numbers.Min());
+
+    }
+
+    numbers.Add(Convert.ToInt32(input));
+}
+*/
+
+
+//Dæmi 2
+
+WeatherData n1 = new WeatherData();
+
+n1.Scale = "C";
+n1.Temperature = 25;
+n1.Humidity = 15;
+
+WeatherData n2 = new WeatherData();
+
+n2.Scale = "F";
+n2.Temperature = 111;
+n2.Humidity = 36;
+
+Console.WriteLine(n1.Temperature);
+Console.WriteLine(n1.Humidity);
+Console.WriteLine(n1.Scale);
+WeatherData.Convert(n1);
+WeatherData.Convert(n1);
+WeatherData.Convert(n2);
+WeatherData.Convert(n2);
---
+++ ConsoleApp1/WeatherData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class WeatherData
+    {
+
+        private string? _Scale;
+        public string? Scale
+        {
+            get { return _Scale; }
+            set
+            {
+                if (value == "C")
+                {
+                    _Scale = "C";
+                }
+                else if (value == "F")
+                {
+                    _Scale = "F";
+                }
+                else
+                {
+                    Console.WriteLine("Please use C for celsius or F for farenheit");
+                }
+            }
+        }
+
+        private double _Temperature;
+
+        public double Temperature
+        {
+            get { return _Temperature; }
+            set
+            {
+                if (_Scale == "C" && value < 60 && value > -60)
+                {
+                    _Temperature = value;
+                }
+                else if (_Scale == "F" && value < 140 && value > -76)
+                {
+                    _Temperature = value;
+                }
+                else
+                {
+                    Console.WriteLine("Temperature value seems unrealistic. A reading mistake must have been made.");
+                }
+            }
+        }
+
+        private int _Humidity;
+
+        public int Humidity
+        {
+            get { return _Humidity; }
+            set
+            {
+                if (value < 100 && value > 0)
+                {
+                    _Humidity = value;
+                }
+                else
+                {
+                    Console.WriteLine("Humidity value seems unrealistic. A reading mistake must have been made.");
+                }
+            }
+        }
+
+       
+
+        public static void Convert(WeatherData weatherData)
+        {
+            Console.WriteLine(weatherData.Temperature + "°" + weatherData.Scale);
+            if (weatherData.Scale == "C")
+            {    
+                double newTemperature = weatherData.Temperature * 9 / 5 + 32;
+                weatherData.Scale = "F";
+                Console.WriteLine(newTemperature + "°" + weatherData.Scale);
+                weatherData._Temperature = newTemperature;
+            } else if (weatherData.Scale == "F")
+            {
+                double newTemperature = (weatherData.Temperature - 32) * 5 / 9;
+                weatherData.Scale = "C";
+                Console.WriteLine(newTemperature + "°" + weatherData.Scale);
+                weatherData._Temperature = newTemperature;
+            }
+        }
+
+    }
+}
